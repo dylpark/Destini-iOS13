2b822d82e0e77f30b0ec084b82ad2a8a0d2ae926
Dylan Park <dylanpark.design@gmail.com>
2021-02-06 14:27:29 +1000

    Update ViewController.swift

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Dylan Park on 06/02/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choiceMade: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
